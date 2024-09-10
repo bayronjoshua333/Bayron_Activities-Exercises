a7a9599eb56ca5052130bd382dd3214a63c7fdf9
bayronjoshua333 <joshua.bayron@awsys-i.com>
2024-09-10 16:48:51 +0800

    Add files via upload

---
+++ Bayron_Activity1 09-03-24/Bayron_L31Activity1/src/Bayron_L31Activity1/Bayron_L31Activity1.java
@@ -0,0 +1,22 @@
+package Bayron_L31Activity1;
+
+public class Bayron_L31Activity1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Joshua Bayron";
+		int age = 15;
+		String country = "the Land of Dawn";
+		String sport = "Nemasu";
+		double hours = 5.5;
+		String games = "Eat";
+		String subject = "Recess";
+		int grade = 100;
+		
+		System.out.println("My name is "+ name + ". I'm "+ age + " years old and from "+ country+".\n "
+				+ "My favorite sport is "+ sport + ". I play for "+ hours + "hour(s) a day \n"
+				+"I like to play "+ games + " when im tired \n"
+				+"In school, my favorite subject was "+ subject+ ". I scored a "+ grade);
+	}
+
+}
