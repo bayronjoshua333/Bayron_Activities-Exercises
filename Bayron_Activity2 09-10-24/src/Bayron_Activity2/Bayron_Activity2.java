a63eeaf260f329ffa00b333dc470fa37f2d3ab50
bayronjoshua333 <joshua.bayron@awsys-i.com>
2024-09-11 20:30:43 +0800

    update j = 0 into j = i + 1;

--- Bayron_Activity2 09-10-24/src/Bayron_Activity2/Bayron_Activity2.java
+++ Bayron_Activity2 09-10-24/src/Bayron_Activity2/Bayron_Activity2.java
@@ -20,7 +20,7 @@ public class Bayron_Activity2 {
 
 	public static int[] findTwoSum(int[] numbers, int target) {
 		for (int i = 0; i < numbers.length; i++) {
-			for (int j = 0; j < numbers.length; j++) {
+			for (int j = i + 1; j < numbers.length; j++) {
 				if (numbers[i] + numbers[j] == target) {
 					return new int[] {i,j};
 				}
