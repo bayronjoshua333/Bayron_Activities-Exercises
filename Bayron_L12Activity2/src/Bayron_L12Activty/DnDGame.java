32e8491eb84acd509af98b416b2eb0b66fc4caf1
bayronjoshua333 <joshua.bayron@awsys-i.com>
2024-10-24 15:45:43 +0800

    Update DnDGame.java

--- Bayron_L12Activity2/src/Bayron_L12Activty/DnDGame.java
+++ Bayron_L12Activity2/src/Bayron_L12Activty/DnDGame.java
@@ -1,10 +1,29 @@
 package Bayron_L12Activty;
 
 public class DnDGame {
+    public static void main(String[] args) {
+        DnDCharacter swordsman = new SwordsMan("Swordsman");
+        DnDCharacter archer = new Archer("Archer");
+        DnDCharacter catalyst = new Catalyst("Catalyst");
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-	}
+        swordsman.attack(archer);
+        archer.attack(swordsman);
+        catalyst.attack(swordsman);
 
+        System.out.println(swordsman);
+        System.out.println(archer);
+        System.out.println(catalyst);
+
+        while (swordsman.isAlive() && archer.isAlive()) {
+            swordsman.attack(archer);
+            if (!archer.isAlive()) {
+                System.out.println(archer.characterName + " is defeated.");
+                break;
+            }
+            archer.attack(swordsman);
+            if (!swordsman.isAlive()) {
+                System.out.println(swordsman.characterName + " is defeated.");
+            }
+        }
+    }
 }
