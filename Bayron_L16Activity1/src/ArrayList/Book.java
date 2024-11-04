84dcdb696fbe9ea0208f63db0b83e86988f357b9
bayronjoshua333 <joshua.bayron@awsys-i.com>
2024-11-04 15:42:03 +0800

    Update Book.java

--- Bayron_L16Activity1/src/ArrayList/Book.java
+++ Bayron_L16Activity1/src/ArrayList/Book.java
@@ -1,5 +1,25 @@
 package ArrayList;
 
 public class Book {
+    String title;
+    boolean isRented;
+    String returnDate;
 
+    // Constructor
+    public Book(String title) {
+        this.title = title;
+        this.isRented = false;
+        this.returnDate = "";
+    }
+
+    // Method to rent the book
+    public void rent(String returnDate) {
+        this.isRented = true;
+        this.returnDate = returnDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + (isRented ? " (Rented, Return Date: " + returnDate + ")" : "");
+    }
 }
