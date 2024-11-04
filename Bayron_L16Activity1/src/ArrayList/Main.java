22be8b7913fec4f7c48c7427dab937274a5e0501
bayronjoshua333 <joshua.bayron@awsys-i.com>
2024-11-04 15:40:43 +0800

    Update Main.java

--- Bayron_L16Activity1/src/ArrayList/Main.java
+++ Bayron_L16Activity1/src/ArrayList/Main.java
@@ -1,10 +1,23 @@
 package ArrayList;
 
+import java.util.ArrayList;
+
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<Book> books = new ArrayList<>();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        // Add books to the library
+        Library.addBooks(books);
 
-	}
+        // Display all books
+        System.out.println("All Books in the Library:");
+        Library.displayBooks(books);
 
+        // Rent books
+        Library.rentBooks(books);
+
+        // Display rented books
+        System.out.println("\nBooks after renting:");
+        Library.displayBooks(books);
+    }
 }
