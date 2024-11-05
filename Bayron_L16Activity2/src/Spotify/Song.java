83488ac0434ad0446c7939dc3019b861fa517050
bayronjoshua333 <joshua.bayron@awsys-i.com>
2024-11-05 13:23:59 +0800

    Update Song.java

--- Bayron_L16Activity2/src/Spotify/Song.java
+++ Bayron_L16Activity2/src/Spotify/Song.java
@@ -1,5 +1,17 @@
 package Spotify;
 
 public class Song {
+    private String title;
+    private String artist;
 
+    // Parameterized constructor
+    public Song(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    // Method to get song details
+    public String getDetails() {
+        return "Title: " + title + ", Artist: " + artist;
+    }
 }
